ac3c6abdf6d5186fa76f92b9072882f561ad4957
nech14 <nech14@mail.ru>
2024-02-11 22:41:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,9 +35,10 @@ def create_mp4(dates, name="output", flag_info=False):
     frame_width = date.shape[1]
     frame_height = date.shape[0]
     fps = 1
-
+    print(date.shape)
     # Создаем объект VideoWriter для записи видео в формате MP4
-    out = cv2.VideoWriter(name+".mp4", cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter(name + ".mp4", fourcc, fps, (frame_width, frame_height), isColor=False)
 
     count = 0
     for i in dates:
@@ -50,7 +51,7 @@ def create_mp4(dates, name="output", flag_info=False):
     out.release()
 
 
-def create_img_for_video(names, start_i, end_i, flag_info=False, name=None):
+def create_img_for_video(names, start_i, end_i, flag_info=False, name=None, cut=False, percent_to_trim=0.1):
     datas = []
     for i in range(start_i, end_i):
         name_path = os.path.join(new_path, names[i])
@@ -58,68 +59,75 @@ def create_img_for_video(names, start_i, end_i, flag_info=False, name=None):
 
         name_path1 = os.path.join(new_path, names[i + 1])
         info1, data1 = file.open_gz(name_path1)
-        img = graphics.create_img_for_video(data, data1, name=name)
+
+        if cut:
+            data_cut = graphics.cut_img(data, percent_to_trim)
+            data1_cut = graphics.cut_img(data1, percent_to_trim)
+            img = graphics.create_img_for_video(data_cut, data1_cut, name=name)
+        else:
+            img = graphics.create_img_for_video(data, data1, name=name)
         # plt.imshow(img)
         # plt.show()
         datas.append(img)
         if flag_info:
-            print(f"create img: {(i-start_i)}/{end_i-start_i-1}")
+            print(f"create img: {(i - start_i)}/{end_i - start_i - 1}")
     return datas
 
 
-def create_video(names, start_i=6, end_i=None, name_file="output", flag_info=False, name=None):
+def create_video(names, start_i=6, end_i=None, name_file="output", flag_info=False, name=None, cut=False):
     if end_i is None:
-        end_i = len(names)-2
-    datas = create_img_for_video(names, start_i=start_i, end_i=end_i, flag_info=flag_info, name=name)
+        end_i = len(names) - 2
+    datas = create_img_for_video(names, start_i=start_i, end_i=end_i, flag_info=flag_info, name=name,cut=cut)
     create_mp4(dates=datas, name=name_file, flag_info=flag_info)
+
+
+def viewing_pictures(names, file_number):
+    name_path = os.path.join(new_path, names[file_number])
+    info, data = file.open_gz(name_path)
+
+    name_path1 = os.path.join(new_path, names[file_number + 1])
+    info1, data1 = file.open_gz(name_path1)
+
+    mode = graphics.print_graphics_cv2_arr(data, data1, data.max(),
+                                           names=[names[file_number][:-8], names[file_number + 1][:-8]])
+    print(names)
+    while True:
+        if mode == 0:
+            break
+        elif mode == 1:
+            file_number -= 1
+        elif mode == 2:
+            file_number += 1
+
+        name_path = os.path.join(new_path, names[file_number])
+        info, data = file.open_gz(name_path)
+
+        name_path1 = os.path.join(new_path, names[file_number + 1])
+        info1, data1 = file.open_gz(name_path1)
+
+        data_cut = graphics.cut_img(data)
+        data1_cut = graphics.cut_img(data1)
+
+        mode = graphics.print_graphics_cv2_arr(data_cut, data1_cut, data_cut.max(),
+                                               names=[names[file_number][:-8], names[file_number + 1][:-8]])
 
 
 current_directory = os.getcwd()
 
-path = os.path.join(current_directory, "data", "ASI0", "2023", "10", "11")
-#"data.ASI0.2023.10.11.5577"
+# path = os.path.join(current_directory, "data", "ASI0", "2023", "10", "11")
+# "data.ASI0.2023.10.11.5577"
 # name="data.ASI0.2024.01.12.5577", name_file="data.ASI0.2024.01.12.5577"
-#path = os.path.join(current_directory, "data", "ASI0", "2024", "01", "12")
+path = os.path.join(current_directory, "data", "ASI0", "2024", "01", "12")
 new_path = os.path.join(path, "5577")
-
 
 names = file.get_name_file(new_path)
 
-name_path = os.path.join(new_path, names[file_number])
-info, data = file.open_gz(name_path)
+#viewing_pictures(names, file_number)
 
-
-name_path1 = os.path.join(new_path, names[file_number+1])
-info1, data1 = file.open_gz(name_path1)
-
-
-#graphics.print_graphics_cv2(combined_image, combined_image.max())
+# graphics.print_graphics_cv2(combined_image, combined_image.max())
 #
-mode = graphics.print_graphics_cv2_arr(data, data1, data.max(), names=[names[file_number][:-8], names[file_number+1][:-8]])
-print(names)
-while True:
-    if mode == 0:
-        break
-    elif mode == 1:
-        file_number -= 1
-    elif mode == 2:
-        file_number += 1
-
-    name_path = os.path.join(new_path, names[file_number])
-    info, data = file.open_gz(name_path)
-
-    name_path1 = os.path.join(new_path, names[file_number+1])
-    info1, data1 = file.open_gz(name_path1)
-
-    data_cut = graphics.cut_img(data)
-    data1_cut = graphics.cut_img(data1)
-
-    mode = graphics.print_graphics_cv2_arr(data_cut, data1_cut, data_cut.max(), names=[names[file_number][:-8], names[file_number+1][:-8]])
 
 
-
-
-#create_video(names, start_i=12, flag_info=True)
-
+#create_video(names, start_i=13, flag_info=True, name_file="data.ASI0.2024.01.12.5577", name="data.ASI0.2024.01.12.5577", cut=True)
 
 print('hay')
