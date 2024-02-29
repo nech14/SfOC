f0bb31aa5f3b87bfba0cce1ccd396809bc5ccd85
nech14 <nech14@mail.ru>
2024-03-01 06:16:02 +0800

    Create fun for video with 3d_hist_diff

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ names = file.get_name_file(new_path)
 
 
 
-def hist_3d(names, file_number, end=10, name_g="", xlim3d_min=0, xlim3d_max=10000, ylim3d_min=0, ylim3d_max=None,rotate=False, x_agnes_end=130, y_agnes_end=90, start_folder="result", save_folder="img"):
+def hist_3d(names, file_number, end=10, name_g="", xlim3d_min=0, xlim3d_max=None, ylim3d_min=0, ylim3d_max=None,rotate=False, x_agnes_end=130, y_agnes_end=90, start_folder="result", save_folder="img"):
 
     #histogram = plt.hist(data_cut.flatten(), bins='auto')
    # print(histogram)
@@ -210,7 +210,8 @@ def hist_3d(names, file_number, end=10, name_g="", xlim3d_min=0, xlim3d_max=1000
     # label every other column number
     if not ylim3d_max is None:
         ax.set_ylim3d(ylim3d_max, ylim3d_min)
-    ax.set_xlim3d(xlim3d_min, xlim3d_max)
+    if not xlim3d_max is None:
+        ax.set_xlim3d(xlim3d_min, xlim3d_max)
 
     plt.title(name_g)
 
@@ -234,6 +235,85 @@ def hist_3d(names, file_number, end=10, name_g="", xlim3d_min=0, xlim3d_max=1000
 
     #plt.show()
 
+
+
+def hist_3d_diff(names, file_number, end=10, name_g="", xlim3d_min=0, xlim3d_max=None, ylim3d_min=0, ylim3d_max=None,rotate=False, x_agnes_end=130, y_agnes_end=90, start_folder="result", save_folder="diff"):
+
+    #histogram = plt.hist(data_cut.flatten(), bins='auto')
+   # print(histogram)
+    #plt.show()
+
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(projection="3d")
+    my_cmap = plt.cm.inferno
+
+    count_files = len(names)-end - 1
+
+    for i in range(file_number, count_files):
+        # we create evenly spaced bins between the minimum and maximum of the entire dataframe
+        name_path = os.path.join(new_path, names[i])
+        info, data = file.open_gz(name_path)
+        data_cut = graphics.cut_img(data)
+
+        name_path1 = os.path.join(new_path, names[i+1])
+        info1, data1 = file.open_gz(name_path1)
+        data_cut1 = graphics.cut_img(data1)
+
+        h_diff = data_cut.astype(float) - data_cut1.astype(float)
+        #non_zero_data_cut = data_cut[data_cut != 0]
+
+        h_diff = h_diff[h_diff != 0]
+
+        histvals, _ = np.histogram(h_diff.flatten(), bins="auto")
+        histvals = histvals[1:]
+
+        xbins = np.linspace(data_cut.flatten().min().min(), data_cut.flatten().max().max(), len(histvals)+1)
+        # and calculate the center and widths of the bars
+        xcenter = np.convolve(xbins, np.ones(2), "valid") / 2
+        xwidth = np.diff(xbins)
+
+        # print(xbins)
+        # print(xcenter)
+
+
+        ax.bar(left=xcenter, height=histvals, width=xwidth, zs=(i-file_number)/100, zdir="y", alpha=0.666, linewidth=0.3, color=my_cmap((i-file_number)/(count_files-file_number)))
+
+        print(f"{i-file_number}/{count_files-file_number}")
+
+
+    ax.set_xlabel("value")
+    ax.set_ylabel("column")
+    ax.set_zlabel("count")
+
+    # label every other column number
+    if not ylim3d_max is None:
+        ax.set_ylim3d(ylim3d_max, ylim3d_min)
+    if not xlim3d_max is None:
+        ax.set_xlim3d(xlim3d_min, xlim3d_max)
+
+    plt.title(name_g)
+
+    if rotate:
+        if not os.path.exists(start_folder+'/'+save_folder):
+            # Если не существует, создаем папку
+            os.makedirs(start_folder+'/'+save_folder)
+        for angle in range(0, x_agnes_end):
+            ax.view_init(0, angle)
+            print(f"rotate_x: {angle}/{x_agnes_end}")
+            #plt.draw()
+            plt.savefig(start_folder + '/' + save_folder + '/rotanim_' + str(angle) + '.png')
+            #plt.pause(.001)
+
+        for angle in range(0, y_agnes_end):
+            ax.view_init(angle, x_agnes_end)
+            print(f"rotate_y: {angle}/{y_agnes_end}")
+            #plt.draw()
+            plt.savefig(start_folder + '/' + save_folder + '/rotanim_' + str(angle+x_agnes_end) + '.png')
+            #plt.pause(.001)
+
+    #plt.show()
+
+
 def create_video_hist_3d(name_file=None, folder_name="result/ASI0/2023/10/11/OH1", save_folder='video'):
     if name_file is None:
         name_file = folder_name
@@ -250,7 +330,6 @@ def create_video_hist_3d(name_file=None, folder_name="result/ASI0/2023/10/11/OH1
     # Создаем объект VideoWriter для записи видео в формате MP4
     fourcc1 = cv2.VideoWriter_fourcc(*'mp4v')
     out1 = cv2.VideoWriter(folder_name + '/../' + save_folder + '/' + name_file + ".mp4", fourcc1, fps, (frame_width, frame_height))
-    print(folder_name + '/../' + save_folder + '/' + name_file + ".mp4")
 
     count = 0
     for i in range(1, 220):
@@ -270,10 +349,13 @@ def create_video_hist_3d(name_file=None, folder_name="result/ASI0/2023/10/11/OH1
 #hist_3d(names, file_number, 20, name_g="data.ASI0.2023.10.11.OH1", xlim3d_max=20000, ylim3d_max=(len(names)-file_number-20)//100, rotate=True, start_folder="result/ASI0/2023/10/11/OH1")
 
 
-#hist_3d(names, file_number, len(names)-20, name_g="data.ASI0.2023.10.11.5577", ylim3d_max=0.5, rotate=True, start_folder="result/ASI0/2023/10/11/5577")
+#hist_3d(names, file_number, 20, name_g="data.ASI0.2023.10.11.5577", ylim3d_max=(len(names)-file_number)/100, rotate=True, start_folder="result/ASI0/2023/10/11/5577")
 
-create_video_hist_3d(name_file="result_ASI0_2023_10_11_5577", folder_name="result/ASI0/2023/10/11/5577/img")
+#create_video_hist_3d(name_file="result_ASI0_2023_10_11_5577", folder_name="result/ASI0/2023/10/11/5577/img")
 
 
+hist_3d_diff(names, file_number, 20, name_g="data.ASI0.2023.10.11.5577.diff", ylim3d_max=(len(names)-file_number)/100, rotate=True, start_folder="result/ASI0/2023/10/11/5577")
+
+create_video_hist_3d(name_file="result_ASI0_2023_10_11_5577_diff", folder_name="result/ASI0/2023/10/11/5577/diff")
 
 print('hay')
