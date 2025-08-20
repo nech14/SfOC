5978aa5da10c46460dbb274428115814eed1dca4
nech14 <nech14@mail.ru>
2025-08-20 12:10:44 +0800

    Update api_tags.py

--- src/api/api_tags.py
+++ src/api/api_tags.py
@@ -1,10 +1,11 @@
 from enum import Enum
 
-tags = ["Здачи", "Редактор", "База данных", "Редактирование"]
+tags = ["Здачи", "Редактор", "База данных", "Редактирование", "Редактирование с БД"]
 
 class ApiTags(Enum):
     Tasks = tags[0]
     Editor = tags[1]
     Database = tags[2]
     Edit = tags[3]
+    EditDatabase = tags[4]
 
